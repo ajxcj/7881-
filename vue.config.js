e7212066ab58a0ca40f963a4c5633facc1e6dcfa
13048331526 <1808149449@qq.com>
2022-06-07 16:45:35 +0800

    初始化成功成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const px2rem = require('postcss-px2rem')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          processors: [
+            px2rem({remUnit: 10}) // 基准大小 baseSize，需要和rem.js中相同
+          ]
+        },
+      }
+    }
+  }
 })
