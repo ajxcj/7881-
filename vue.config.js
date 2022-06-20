414d35eebd81c8acd0f7ef5bb5a4bc43f2855104
13452014004 <1597237414@qq.com>
2022-06-20 18:42:17 +0800

    上传

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 const px2rem = require('postcss-px2rem')
 module.exports = defineConfig({
+  publicPath: './',
   transpileDependencies: true,
   css: {
     loaderOptions: {
       postcss: {
         postcssOptions: {
           processors: [
-            px2rem({remUnit: 10}) // 基准大小 baseSize，需要和rem.js中相同
+            px2rem({ remUnit: 10 }) // 基准大小 baseSize，需要和rem.js中相同
           ]
         },
       }
     }
   },
-  devServer:{
-    proxy:{
-      '/home':{
-          target:'https://gw.7881.com',
-          pathRewrite:{ //重写路径，剔除多余的代理路径
-            '^/home':''
-          }  
+  devServer: {
+    proxy: {
+      '/home': {
+        target: 'https://gw.7881.com',
+        pathRewrite: { //重写路径，剔除多余的代理路径
+          '^/home': ''
+        }
       }
     }
   }
